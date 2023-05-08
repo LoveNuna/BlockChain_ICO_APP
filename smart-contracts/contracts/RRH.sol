a1aa463d4da8b35187d65be517d2f251e5e97823
marybarnett12 <marybarnett120@gmail.com>
2023-05-08 15:39:33 -0800

    change deploy.js

---
+++ smart-contracts/contracts/RRH.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RRH is ERC20 {
+    constructor() ERC20("RiRiHam Token", "RRH") {
+        _mint(msg.sender, 5000 * 1e18);
+    }
+}
