817e021a44cfe8ecbe9924e3723e859b24be7dca
marybarnett12 <marybarnett120@gmail.com>
2023-05-08 16:01:23 -0800

    deploy successfuly

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -8,5 +8,10 @@ module.exports = {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
     },
+    hardhat: {
+      forking: {
+        url: "https://mainnet.infura.io/v3/87f0a1e5d26e4eddb8c310624a77a8d6",
+      }
+    }
   },
 };
